ae1d376a98d14e4b7ce94c1fffa317053ba95d8d
andrewhamilton0 <acjts425@gmail.com>
2022-04-13 15:00:37 -0400

    Adds internet permission as well as open weather api. Adds asynchronous thread through OKHttp. Adds dialog box if error occurs with URL.

--- app/src/main/java/com/andrew/stormytutorial/MainActivity.java
+++ app/src/main/java/com/andrew/stormytutorial/MainActivity.java
@@ -3,12 +3,76 @@ package com.andrew.stormytutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    String apiKey = "3ab49fc3c2b6e93bf90275c0df750f11";
+    double latitude = 37.8267 ;
+    double longitude = -122.4233;
+    String forecastUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude +
+            "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+
+    private final OkHttpClient client = new OkHttpClient();
+
+
+
+    public void run() throws Exception {
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                System.out.println("POOP");
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()){
+
+                        }
+                        else{
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    }
+                }
+            }
+        });
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
+
+
 }
